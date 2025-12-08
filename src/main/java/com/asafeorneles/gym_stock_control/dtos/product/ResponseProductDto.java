6c33b2d0765e134468451bde8958e52048bacf1c
Asafe Orneles <asafeorneles@gmail.com>
2025-12-08 15:06:58 -0300

    refactor: I improved the ResponseProductDto to return the id in the responses.

--- src/main/java/com/asafeorneles/gym_stock_control/dtos/product/ResponseProductDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/product/ResponseProductDto.java
@@ -3,8 +3,10 @@ package com.asafeorneles.gym_stock_control.dtos.product;
 import com.asafeorneles.gym_stock_control.dtos.category.ResponseCategoryDto;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record ResponseProductDto(
+        UUID productId,
         String name,
         String brand,
         BigDecimal price,
@@ -12,6 +14,5 @@ public record ResponseProductDto(
         ResponseCategoryDto responseCategoryDto,
         int quantity,
         int lowStockThreshold
-
 ) {
 }
