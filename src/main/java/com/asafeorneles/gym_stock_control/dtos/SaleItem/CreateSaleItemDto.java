26ddee2a91d27f9a5473242ba29c4b8441b2674e
Asafe Orneles <asafeorneles@gmail.com>
2025-12-15 20:49:56 -0300

    refactor: I included the validations in CreateSaleDto and CreateSaleItemDto.

--- src/main/java/com/asafeorneles/gym_stock_control/dtos/SaleItem/CreateSaleItemDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/SaleItem/CreateSaleItemDto.java
@@ -1,9 +1,14 @@
 package com.asafeorneles.gym_stock_control.dtos.SaleItem;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record CreateSaleItemDto (
+public record CreateSaleItemDto(
+        @NotNull(message = "The productId cannot be null!")
         UUID productId,
+        @Min(value = 1, message = "The quantity cannot be less than 1!")
         int quantity
-){
+) {
 }
--- src/main/java/com/asafeorneles/gym_stock_control/dtos/sale/CreateSaleDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/sale/CreateSaleDto.java
@@ -2,12 +2,16 @@ package com.asafeorneles.gym_stock_control.dtos.sale;
 
 import com.asafeorneles.gym_stock_control.dtos.SaleItem.CreateSaleItemDto;
 import com.asafeorneles.gym_stock_control.enums.PaymentMethod;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record CreateSaleDto(
         @NotNull(message = "The list of sale items cannot be null!")
+        @Size(min = 1, message = "The sale must have at least one item")
+        @Valid
         List<CreateSaleItemDto> saleItems,
         @NotNull(message = "The payment method of sale items cannot be null!")
         PaymentMethod paymentMethod
