72cb25a690e75b7c544d1c6cb1e98213169c54bb
Asafe Orneles <asafeorneles@gmail.com>
2025-12-17 17:46:45 -0300

    test: I create the test for createSale method

---
+++ src/test/java/com/asafeorneles/gym_stock_control/services/SaleServiceTest.java
@@ -0,0 +1,172 @@
+package com.asafeorneles.gym_stock_control.services;
+
+import com.asafeorneles.gym_stock_control.dtos.SaleItem.CreateSaleItemDto;
+import com.asafeorneles.gym_stock_control.dtos.SaleItem.ResponseSaleItemDto;
+import com.asafeorneles.gym_stock_control.dtos.sale.CreateSaleDto;
+import com.asafeorneles.gym_stock_control.dtos.sale.ResponseSaleDto;
+import com.asafeorneles.gym_stock_control.entities.Category;
+import com.asafeorneles.gym_stock_control.entities.Product;
+import com.asafeorneles.gym_stock_control.entities.Sale;
+import com.asafeorneles.gym_stock_control.entities.SaleItem;
+import com.asafeorneles.gym_stock_control.enums.PaymentMethod;
+import com.asafeorneles.gym_stock_control.repositories.ProductRepository;
+import com.asafeorneles.gym_stock_control.repositories.SaleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.ErrorResponseException;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SaleServiceTest {
+
+    @Mock
+    SaleRepository saleRepository;
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    ProductInventoryService productInventoryService;
+
+    @InjectMocks
+    SaleService saleService;
+
+    private SaleItem saleItem;
+    private Sale sale;
+    private CreateSaleDto createSaleDto;
+    private CreateSaleItemDto createSaleItemDto;
+    @Captor
+    ArgumentCaptor<Sale> saleCaptor;
+
+    @BeforeEach
+    void setUp() {
+        Category category = Category.builder()
+                .categoryId(UUID.randomUUID())
+                .name("Suplementos")
+                .description("Alimento em pó para maior eficiência")
+                .build();
+
+        Product product = Product.builder()
+                .productId(UUID.randomUUID())
+                .name("Hipercalórico")
+                .brand("Growth")
+                .price(BigDecimal.valueOf(100.99))
+                .costPrice(BigDecimal.valueOf(69.99))
+                .category(category)
+                .build();
+        saleItem = SaleItem.builder().saleItemId(UUID.randomUUID())
+                .product(product)
+                .quantity(5)
+                .costPrice(product.getCostPrice())
+                .unityPrice(product.getPrice())
+                .totalPrice(product.getPrice().multiply(BigDecimal.valueOf(5)))
+                .build();
+
+        sale = Sale.builder().saleId(UUID.randomUUID())
+                .saleItems(List.of(saleItem))
+                .paymentMethod(PaymentMethod.PIX).build();
+
+        saleItem.setSale(sale);
+
+        sale.calculateTotalPrice();
+
+        createSaleItemDto = new CreateSaleItemDto(product.getProductId(), 5);
+        createSaleDto = new CreateSaleDto(List.of(createSaleItemDto), PaymentMethod.PIX);
+    }
+
+    @Nested
+    class createSale {
+        @Test
+        void shouldCreateSaleWithSuccess(){
+            // ARRANGE
+            when(productRepository.findById(any(UUID.class)))
+                    .thenReturn(Optional.of(saleItem.getProduct()));
+
+            when(saleRepository.save(any(Sale.class)))
+                    .thenReturn(sale);
+
+            // ACT
+            ResponseSaleDto responseSaleDto = saleService.createSale(createSaleDto);
+
+            // ASSERT
+            verify(saleRepository).save(saleCaptor.capture());
+            Sale saleSaved = saleCaptor.getValue();
+
+            assertEquals(1, saleSaved.getSaleItems().size());
+            assertEquals(PaymentMethod.PIX, saleSaved.getPaymentMethod());
+            assertEquals(sale.getTotalPrice(), saleSaved.getTotalPrice());
+
+            assertNotNull(responseSaleDto);
+            assertEquals(sale.getSaleItems().size(), responseSaleDto.saleItems().size());
+            assertEquals(sale.getTotalPrice(), responseSaleDto.totalPrice());
+            assertEquals(sale.getPaymentMethod(), responseSaleDto.paymentMethod());
+
+            verify(productRepository).findById(createSaleItemDto.productId());
+
+            verify(productInventoryService, times(1)).updateQuantityAfterSale(anyList());
+        }
+        @Test
+        void shouldThrowExceptionWhenProductDoesNotExist() {
+            when(productRepository.findById(createSaleItemDto.productId()))
+                    .thenReturn(Optional.empty());
+
+            // ASSERT
+            assertThrows(ErrorResponseException.class,() -> saleService.createSale(createSaleDto));
+
+            verify(productInventoryService, never()).updateQuantityAfterSale(any());
+
+            verify(saleRepository, never()).save(any(Sale.class));
+        }
+
+        @Test
+        void shouldNotSaveSaleWhenInventoryUpdateFails() {
+            // ARRANGE
+            when(productRepository.findById(createSaleItemDto.productId()))
+                    .thenReturn(Optional.of(saleItem.getProduct()));
+
+            doThrow(new RuntimeException("Inventory error"))
+                    .when(productInventoryService)
+                    .updateQuantityAfterSale(any());
+
+            // ASSERT
+            assertThrows(RuntimeException.class, () -> saleService.createSale(createSaleDto));
+
+            verify(saleRepository, never()).save(any());
+        }
+    }
+
+    @Test
+    void newSaleItemList() {
+    }
+
+    @Test
+    void findSales() {
+    }
+
+    @Test
+    void findSaleById() {
+    }
+
+    @Test
+    void deleteSale() {
+    }
+
+    @Test
+    void updatePaymentMethod() {
+    }
+}
