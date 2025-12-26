3d2d01bce360bcd2e70ff6c48e5001688fe05c0c
Asafe Orneles <asafeorneles@gmail.com>
2025-12-26 19:57:38 -0300

    feat: I created the CouponRepository and CouponController

---
+++ src/main/java/com/asafeorneles/gym_stock_control/controllers/CouponController.java
@@ -0,0 +1,10 @@
+package com.asafeorneles.gym_stock_control.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coupon")
+public class CouponController {
+
+}
---
+++ src/main/java/com/asafeorneles/gym_stock_control/repositories/CouponRepository.java
@@ -0,0 +1,11 @@
+package com.asafeorneles.gym_stock_control.repositories;
+
+import com.asafeorneles.gym_stock_control.entities.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
