b52ecf7fc954d2962a8526fc8e484e1c38b12ef6
Asafe Orneles <asafeorneles@gmail.com>
2025-12-27 04:31:07 -0300

    refactor: I added the attribute CouponAppliedDto in the method saleToResponseSale().

--- src/main/java/com/asafeorneles/gym_stock_control/mapper/SaleMapper.java
+++ src/main/java/com/asafeorneles/gym_stock_control/mapper/SaleMapper.java
@@ -1,5 +1,6 @@
 package com.asafeorneles.gym_stock_control.mapper;
 
+import com.asafeorneles.gym_stock_control.dtos.coupon.CouponAppliedDto;
 import com.asafeorneles.gym_stock_control.dtos.sale.CreateSaleDto;
 import com.asafeorneles.gym_stock_control.dtos.sale.ResponseSaleDto;
 import com.asafeorneles.gym_stock_control.entities.Sale;
@@ -12,12 +13,25 @@ public class SaleMapper {
     }
 
     public static ResponseSaleDto saleToResponseSale(Sale sale) {
+        if (sale.containsCoupon()) {
+            return new ResponseSaleDto(
+                    sale.getSaleId(),
+                    SaleItemMapper.saleItemsToResponseSaleItems(sale.getSaleItems()),
+                    new CouponAppliedDto(sale.getCoupon().getCode(), sale.getDiscountAmount()),
+                    sale.getTotalPrice(),
+                    sale.getPaymentMethod(),
+                    sale.getCreatedDate()
+            );
+        }
+
         return new ResponseSaleDto(
                 sale.getSaleId(),
                 SaleItemMapper.saleItemsToResponseSaleItems(sale.getSaleItems()),
+                null,
                 sale.getTotalPrice(),
                 sale.getPaymentMethod(),
                 sale.getCreatedDate()
         );
     }
 }
+
