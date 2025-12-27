30215743881c5ee00a258e9bf213ae84bbbd69d7
Asafe Orneles <asafeorneles@gmail.com>
2025-12-27 04:28:38 -0300

    feat: I created the InvalidCouponException to handler "Invalid Coupon" type Exceptions.

---
+++ src/main/java/com/asafeorneles/gym_stock_control/exceptions/InvalidCouponException.java
@@ -0,0 +1,7 @@
+package com.asafeorneles.gym_stock_control.exceptions;
+
+public class InvalidCouponException extends RuntimeException {
+    public InvalidCouponException(String message) {
+        super(message);
+    }
+}
