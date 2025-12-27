d6b374804d7cc7f48b63af9d0c725ff6f758f258
Asafe Orneles <asafeorneles@gmail.com>
2025-12-27 20:16:01 -0300

    feat: I added the attributes statusActivity and createdDate.

--- src/main/java/com/asafeorneles/gym_stock_control/entities/Category.java
+++ src/main/java/com/asafeorneles/gym_stock_control/entities/Category.java
@@ -1,5 +1,8 @@
 package com.asafeorneles.gym_stock_control.entities;
 
+import com.asafeorneles.gym_stock_control.enums.ActivityStatus;
+import com.asafeorneles.gym_stock_control.exceptions.CouponAlreadyActiveException;
+import com.asafeorneles.gym_stock_control.exceptions.ProductAlreadyInactivityException;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -25,8 +28,34 @@ public class Category {
     @Column(name = "description")
     private String description;
 
+    @Column(name = "activity_status")
+    @Enumerated(EnumType.STRING)
+    private ActivityStatus activityStatus;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
     @Column(name = "updated_date")
     private LocalDateTime updatedDate;
+
+    public void inactivity(){
+        if (this.activityStatus == ActivityStatus.INACTIVITY){
+            throw new ProductAlreadyInactivityException("Category is already inactive!");
+        }
+        this.activityStatus = ActivityStatus.INACTIVITY;
+    }
+
+    public void activity(){
+        if (this.activityStatus == ActivityStatus.ACTIVE){
+            throw new CouponAlreadyActiveException("Category is already active!");
+        }
+        this.activityStatus = ActivityStatus.ACTIVE;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        this.createdDate = LocalDateTime.now();
+    }
 
     @PreUpdate
     public void preUpdate() {
