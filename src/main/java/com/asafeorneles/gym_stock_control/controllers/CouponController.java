e7bb5cbda4a7efad576cddb633b8773189270640
Asafe Orneles <asafeorneles@gmail.com>
2025-12-27 04:15:13 -0300

    feat: I created the endpoints createCoupon and getAllCoupons.

--- src/main/java/com/asafeorneles/gym_stock_control/controllers/CouponController.java
+++ src/main/java/com/asafeorneles/gym_stock_control/controllers/CouponController.java
@@ -1,7 +1,15 @@
 package com.asafeorneles.gym_stock_control.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.asafeorneles.gym_stock_control.dtos.coupon.CreateCouponDto;
+import com.asafeorneles.gym_stock_control.dtos.coupon.ResponseCouponDto;
+import com.asafeorneles.gym_stock_control.services.CouponService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/coupon")
