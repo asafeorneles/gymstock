3de491f31253bc06fb37bb9e2af41f5c05edd66e
Asafe Orneles <asafeorneles@gmail.com>
2025-12-27 04:32:50 -0300

    feat: I created the migrations to create a new table "tb_coupon" and add the coupon_id and discount_amount columns to the sales table.

---
+++ src/main/resources/db/migration/V3__create_table_coupon_and_add_column_coupon_to__sale.sql
@@ -0,0 +1,21 @@
+CREATE TABLE tb_coupon
+(
+    coupon_id       BINARY(16) PRIMARY KEY,
+    code            VARCHAR(30)    NOT NULL,
+    description     VARCHAR(255)   NOT NULL,
+    discount_value  DECIMAL(38, 2) NOT NULL,
+    discount_type   VARCHAR(30)    NOT NULL,
+    unlimited       BOOLEAN        NOT NULL DEFAULT FALSE,
+    quantity        INT            NOT NULL,
+    activity_status VARCHAR(30)    NOT NULL,
+    expiration_date DATETIME,
+    created_date    DATETIME       NOT NULL,
+    updated_date    DATETIME,
+    CONSTRAINT uk_product_name_brand UNIQUE (code)
+);
+
+ALTER TABLE tb_sales
+    ADD COLUMN discount_amount DECIMAL(38, 2),
+    ADD COLUMN coupon_id BINARY(16),
+    ADD CONSTRAINT fk_sale_coupon
+        FOREIGN KEY (coupon_id) REFERENCES tb_coupon (coupon_id)
