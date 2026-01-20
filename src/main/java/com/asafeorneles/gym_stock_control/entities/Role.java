9870babe65184277992771d3281b694f9ba9d6b2
Asafe Orneles <asafeorneles@gmail.com>
2026-01-20 16:53:12 -0300

    feat: I added the AllArgConstructor in the Role entity to facilitate testing.

--- src/main/java/com/asafeorneles/gym_stock_control/entities/Role.java
+++ src/main/java/com/asafeorneles/gym_stock_control/entities/Role.java
@@ -1,6 +1,7 @@
 package com.asafeorneles.gym_stock_control.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
