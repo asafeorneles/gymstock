d6bba3da44b5fb5aa22ba6037d5b6cf8f0b4754c
Asafe Orneles <asafeorneles@gmail.com>
2026-01-23 18:58:50 -0300

    Refactor: I added the refresh token variable in the LoginResponseDto.

--- src/main/java/com/asafeorneles/gym_stock_control/dtos/auth/LoginResponseDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/auth/LoginResponseDto.java
@@ -1,4 +1,8 @@
 package com.asafeorneles.gym_stock_control.dtos.auth;
 
-public record LoginResponseDto(String token, Long expiresIn) {
+public record LoginResponseDto(
+        String token,
+        String refreshToken,
+        Long expiresIn
+) {
 }
