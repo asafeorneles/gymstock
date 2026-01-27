076c184c3fe843dcf3b78f4b2327b2a4b8260f76
Asafe Orneles <asafeorneles@gmail.com>
2026-01-27 01:05:18 -0300

    refactor: I corrected the error messages in the DTO validations.

--- src/main/java/com/asafeorneles/gym_stock_control/dtos/auth/FirstAdminDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/auth/FirstAdminDto.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Size;
 
 public record FirstAdminDto(
         @NotBlank(message = "The username cannot be empty!")
-        @Size(min = 4, max = 100, message = "The username should be between 2 and 100 characters")
+        @Size(min = 4, max = 100, message = "The username should be between 4 and 100 characters")
         @Pattern(regexp = "^[a-zA-Z0-9._-]+$", message = "The username must contain only letters, numbers, periods, dashes, or underscores.")
         String username,
         @NotBlank(message = "The password cannot be empty!")
-        @Size(min = 4, max = 100, message = "The password should be between 2 and 100 characters")
+        @Size(min = 4, max = 100, message = "The password should be between 4 and 100 characters")
 //        @Pattern(
 //                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
 //                message = "The password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
